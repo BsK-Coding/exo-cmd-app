9a27ee4084d2db43ae8f2a6f8428bd941f75353b
BsK-Coding <blackshark.blog@gmail.com>
2021-03-19 18:55:19 +0100

    maj v1.1

--- guessNumber.js
+++ guessNumber.js
@@ -10,26 +10,65 @@ si le nombre entré par l'utilisateur est égal au nombre secret: 'Bravo!' en ve
 /* Pseudo Code */
 // while (true) => permet de bouclé le programme tant que l'utilisateur ne trouve pas le nombre entré en commande
 // initialisation de variables: nb / nbSecret(taper dans la commande)
+// Check conditions:
+// IF nb != d'un nombre = console.log(chalk.red('Vous devez entrer un nombre'))
 // IF nb < nbSecret = console.log(Nombre trop petit) écrit en rouge (chalk)
 // IF nb > nbSecret = console.log(Nombre trop grand) écrit en rouge
 // IF nb == nbSecret = console.log(Bravo!) écrit en vert
+
 
 /* Import de Librairie utiles au programme */
 
 const readlineSync = require('readline-sync')
 const chalk = require('chalk')
 
-const NbMagic = (nbSecret, nb) => {
-  while (true) {
-    if (nb < nbSecret) {
-      console.log(chalk.red('Nombre trop petit'))
-    }
-    if (nb > nbSecret) {
-      console.log(chalk.red('Nombre trop grand'))
+/* fonction */
+
+const NbMagic = (boucle, count, nbSecret, nb) => {
+
+  /* Check nombre entrer => nbStr */
+
+  if (isNaN(nbStr)) {
+    console.log(chalk.red('!! Vous devez entrer un nombre !!'))
+    process.exit(1)
+  }
+  else {
+    while (boucle) {
+      if (nb < nbSecret) {
+        console.log(chalk.red('Nombre trop petit'))
+        process.exit(1)
+      }
+      if (nb > nbSecret) {
+        console.log(chalk.red('Nombre trop grand'))
+        process.exit(1)
+      }
+      else {
+        console.log(chalk.green('Bravo!'))
+        process.exit(1)                             // On pourrait également mettre "return boucle = false"
+      }
     }
   }
 }
 
 const nbSecret = Number(process.argv[2])
-const nb = readlineSync.question('Entrer un nombre: ')
-NbMagic(nbSecret, nb)
+
+/* Check Command Line */
+if (process.argv.length != 3) {
+  console.log('La commande doit avoir 3 arguments: node guessNumber nbSecret')
+  process.exit(1)
+}
+
+if (isNaN(process.argv[2])) {
+  console.log(chalk.red('!! Vous devez entrer un nombre !!'))
+  process.exit(1)
+}
+
+let boucle = true
+let count = 0
+const nbStr = readlineSync.question('Entrer un nombre: ')
+const nb = Number(nbStr)
+
+
+NbMagic(boucle, count, nbSecret, nb)
+
+/* !! Reste à régler la boucle tant que l'utilisateur ne trouve pas le bon nombre remplacer temporairement par process.exit(1) pour terminer la boucle !! */
