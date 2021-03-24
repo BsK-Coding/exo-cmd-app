b97ad0586d26aa619360298e2bae06eda832cb63
BsK-Coding <blackshark.blog@gmail.com>
2021-03-24 12:29:57 +0100

    maj

--- pyramidAmeliore.js
+++ pyramidAmeliore.js
@@ -19,15 +19,15 @@ const pyramid = (nbElem, charElem, reverse) => {
 }
 
 /* Boucles IF qui Test les erreurs */
-// 4 arguments 1=node 2=programme 3=argument (nbElem) 4=argument (charElem)
+// 5 arguments 1=node 2=programme 3=argument (nbElem) 4=argument (charElem) 5=argument -r (Reverse)
 
-if (process.argv.length != 5) {
-  console.log('La commande doit avoir 4 arguments: node pyramid.js nombreElement motif')
+if (process.argv.length < 4 || process.argv.length > 5) {
+  console.log('La commande doit avoir 5 arguments: node pyramid.js nombreElement motif reverse')
   process.exit(1)
 }
 
 /* Boucles IF qui Test l'argument 5 */
-if ((process.argv[4]) !== '-r') {
+if (process.argv.length === 5 && process.argv[4] !== '-r') {
   console.log("cet argument d'inversement de pyramid est incorrecte")
   process.exit(1)
 }
@@ -38,16 +38,11 @@ if (isNaN(process.argv[2])) {
   process.exit(1)
 }
 
-/* Boucle basé sur les indexs */
-if (!isNaN(process.argv[3])) {
-  console.log(`Error: ${process.argv[3]} motif doit être un caractere`)
-  process.exit(1)
-}
-
 /* argument entré à partir de l'index 3 */
 /* Index 0 et 1 et dédié à node et programme lancé */
 
-let reverse = "-r"
 let nbElem = Number(process.argv[2])
 let charElem = process.argv[3]
-pyramid(nbElem, charElem, reverse)
+let isReverse = process.argv.length === 5 ? true : false
+
+pyramid(nbElem, charElem, isReverse)
